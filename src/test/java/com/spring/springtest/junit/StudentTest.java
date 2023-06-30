46e949ea4fd945db8df1f2f90036df2f63df3919
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-06-30 20:06:11 +0200

    assertAll

--- src/test/java/com/spring/springtest/junit/StudentTest.java
+++ src/test/java/com/spring/springtest/junit/StudentTest.java
@@ -4,22 +4,29 @@ import org.junit.jupiter.api.*;
 
 class StudentTest {
 
-    private Student student;
-
-    @BeforeEach
-    void setUp() {
-        student = new Student();
-    }
-
     @Test
-    void getName() {
-        //Assertions.assertEquals(student.getName(),"eslam","Name not Matches");
-        Assertions.assertTrue("Abdallha".equals(student.getName()),() -> "Name not Matches");
-    }
-
-    @Test
-    void getAge() {
-        //Assertions.assertEquals(student.getAge(),"22","Age not Matches");
-        Assertions.assertTrue("22".equals(student.getAge()),() -> "Age not Matches");
+    void testing(){
+        Student student = new Student();
+        Assertions.assertAll
+                ("Student Data",
+                        () -> Assertions.assertEquals(student.getName(), "Abdallha", "Name not Match"),
+                        () -> Assertions.assertEquals(student.getAge(), "22", "Age Not Match")
+                );
     }
 }
+//    @BeforeEach
+//    void setUp() {
+//        student = new Student();
+//    }
+//
+//    @Test
+//    void getName() {
+//        //Assertions.assertEquals(student.getName(),"eslam","Name not Matches");
+//        Assertions.assertTrue("Abdallha".equals(student.getName()),() -> "Name not Matches");
+//    }
+//
+//    @Test
+//    void getAge() {
+//        //Assertions.assertEquals(student.getAge(),"22","Age not Matches");
+//        Assertions.assertTrue("22".equals(student.getAge()),() -> "Age not Matches");
+//    }
