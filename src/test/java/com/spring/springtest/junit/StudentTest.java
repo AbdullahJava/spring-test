4fa21b5dfb3a8f50729aa368585b45b7d0f9d50b
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-07-01 00:58:34 +0200

    @disabled

--- src/test/java/com/spring/springtest/junit/StudentTest.java
+++ src/test/java/com/spring/springtest/junit/StudentTest.java
@@ -5,52 +5,9 @@ import org.junit.jupiter.api.*;
 class StudentTest {
 
     @Test
-    void personTest(){
-        Person person = new Person();
-//        Assertions.assertAll
-//                ("Student Data",
-//                        () -> Assertions.assertEquals(person.getFName(),"Eslam","F Name not Matches"),
-//                        () -> Assertions.assertEquals(person.getLName(),"Khder","L Name  not Matches"),
-//                        () -> Assertions.assertEquals(person.getPhone1(),"01113903660","Phone not Matches"),
-//                        () -> Assertions.assertEquals(person.getPhone2(),"01255582855","Phone  not Matches")
-//                );
-
-        Assertions.assertAll("Person Data",
-                () -> Assertions.assertAll
-                        ("Person Names",
-                                () -> Assertions.assertEquals(person.getFName(),"Eslam","F Name not Matches"),
-                                () -> Assertions.assertEquals(person.getLName(),"Khder","L Name  not Matches")
-                        ),
-                () -> Assertions.assertAll
-                        ("Person Phones",
-                                () -> Assertions.assertEquals(person.getPhone1(),"01113903660","Phone not Matches"),
-                                () -> Assertions.assertEquals(person.getPhone2(),"01255582855","Phone  not Matches")
-                        )
-        );
+    void findById() {
+        System.out.println("Student 3333");
+        Student student = new Student();
+        Assertions.assertEquals(student.findById(5), "Eslam");
     }
-//    @Test
-//    void testing(){
-//        Student student = new Student();
-//        Assertions.assertAll
-//                ("Student Data",
-//                        () -> Assertions.assertEquals(student.getName(), "Abdallha", "Name not Match"),
-//                        () -> Assertions.assertEquals(student.getAge(), "22", "Age Not Match")
-//                );
-//    }
 }
-//    @BeforeEach
-//    void setUp() {
-//        student = new Student();
-//    }
-//
-//    @Test
-//    void getName() {
-//        //Assertions.assertEquals(student.getName(),"eslam","Name not Matches");
-//        Assertions.assertTrue("Abdallha".equals(student.getName()),() -> "Name not Matches");
-//    }
-//
-//    @Test
-//    void getAge() {
-//        //Assertions.assertEquals(student.getAge(),"22","Age not Matches");
-//        Assertions.assertTrue("22".equals(student.getAge()),() -> "Age not Matches");
-//    }
